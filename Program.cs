1fc5ef437bfcf22d31b1fdc7ef5cbefbf1d069ad
B-Valle <walerybaeva@gmail.com>
2021-11-15 04:51:33 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Valtec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Valtec.scr.Connect.OpenCOMPort();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ scr/Connect.cs
@@ -0,0 +1,19 @@
+﻿using System.IO.Ports;
+
+namespace Valtec.scr
+{
+    public class Connect
+    {
+        public static void OpenCOMPort()
+        {
+            var comPort = new SerialPort();
+            comPort.PortName = "COM4";
+            comPort.BaudRate = 9600;
+            comPort.DataBits = 8;
+            comPort.Parity = Parity.None;
+            comPort.StopBits = StopBits.One;
+            comPort.Open();
+            comPort.Close();
+        }
+    }
+}
